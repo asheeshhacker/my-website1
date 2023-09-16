c30d433ef533c360e1f484985f5d35a6e3c1317a
asheesh anuroop <47481292+asheeshhacker@users.noreply.github.com>
2023-09-17 00:02:55 +0530

    settings.py

--- mysite1/settings.py
+++ mysite1/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-d0a4d7cc!a00goxjmt1a4n5&0uv3^k$$8d6q0j(37!+a2v=n+%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercal.app']
 
 
 # Application definition
